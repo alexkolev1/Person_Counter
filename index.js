22da55789a6369db56a2c9e12e2aab32cff5459f
Alex <alex.kolev@gmail.com>
2024-02-08 12:37:36 -0500

    Added button that increments by 1

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.getElementById("count-el").innerText = 5
+
+//let count = 5
+
+//count = count + 1
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    document.getElementById("count-el").innerText = count
+}
+
+
+
+
+
+
