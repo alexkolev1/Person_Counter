bbe5ac8e5caa7b957408ad46fda7f5893be17180
Alex <alex.kolev@gmail.com>
2024-02-08 17:06:40 -0500

    added a save button

--- index.js
+++ index.js
@@ -5,10 +5,22 @@
 //count = count + 1
 
 let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-    count = count + 1
-    document.getElementById("count-el").innerText = count
+    count += 1
+    countEl.innerText = count
+}
+
+
+
+function save(){
+    let countStr = count + " - "
+
+    saveEl.innerText += countStr
+
+    console.log(count)
 }
 
 
